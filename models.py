e9ec674cbaccbfdb2fd8eeeb754fdd9c0db3850d
Jésica Collado <collado.jesica@gmail.com>
2019-12-31 19:05:59 -0300

    Add Species model and stats calculation

--- models.py
+++ models.py
@@ -4,23 +4,63 @@ from dataclasses import dataclass, field
 
 
 @dataclass(frozen=True)
+class Species:
+    name: str
+    hp: int
+    attack: int
+    defense: int
+    sp_attack: int
+    sp_defense: int
+    speed: int
+
+
+@dataclass(frozen=True)
 class Move:
     name: str
     power: int
 
 
-@dataclass
 class Pokemon:
-    nickname: str
-    hp: int
-    attack: int
-    defense: int
-    moves: set = field(default_factory=set)
+    def __init__(self, nickname: str, species: Species, level: int, moves: set=None):
+        self.nickname = nickname
+        self.species = species
+        self.level = level
+        self.hp = self.max_hp
+        self.moves = moves if moves else {}
+
+    def _calculate_stats(self, base):
+        return math.floor(5 + base * 2 * self.level / 100)
+
+    @property
+    def max_hp(self):
+        return math.floor(10 + self.level + 2 * self.species.hp * self.level / 100)
+
+    @property
+    def attack(self):
+        return self._calculate_stats(self.species.attack)
+
+    @property
+    def defense(self):
+        return self._calculate_stats(self.species.defense)
+
+    @property
+    def sp_attack(self):
+        return self._calculate_stats(self.species.sp_attack)
+
+    @property
+    def sp_defense(self):
+        return self._calculate_stats(self.species.sp_defense)
+
+    @property
+    def speed(self):
+        return self._calculate_stats(self.species.speed)
 
     def receive_damage(self, damage):
         self.hp = self.hp - damage
 
     def perform_move_against(self, move, other_pokemon):
-        damage = math.floor(move.power * self.attack / other_pokemon.defense) + 2
+        level_factor = 2 + 2 * self.level / 5
+        attack_defense_ratio = self.attack / other_pokemon.defense
+        damage = math.floor(level_factor * move.power * attack_defense_ratio / 50) + 2
         other_pokemon.receive_damage(damage)
         return damage
--- test_models.py
+++ test_models.py
@@ -1,29 +1,73 @@
+import pytest
+
 import models
 
-def test_pokemon_receives_damage_and_looses_hp():
-    pikachu = models.Pokemon(
+@pytest.fixture
+def pikachu():
+    return models.Species(
         'Pikachu',
         hp=35,
         attack=55,
         defense=30,
+        sp_attack=50,
+        sp_defense=40,
+        speed=90,
     )
 
-    pikachu.receive_damage(10)
+@pytest.fixture
+def squirtle():
+    return models.Species(
+        'Squirtle',
+        hp=44,
+        attack=48,
+        defense=65,
+        sp_attack=50,
+        sp_defense=64,
+        speed=43,
+    )
 
-    assert pikachu.hp == 25
+def test_pokemon_calculates_stats_properly(pikachu):
+    spark = models.Pokemon(
+        'Spark',
+        pikachu,
+        level=20,
+    )
 
-def test_pokemon_attacks_other_pokemon_and_makes_damage():
+    assert spark.max_hp == 44
+    assert spark.attack == 27
+    assert spark.defense == 17
+    assert spark.sp_attack == 25
+    assert spark.sp_defense == 21
+    assert spark.speed == 41
+
+def test_pokemon_initializes_healthy(pikachu):
+    spark = models.Pokemon(
+        'Spark',
+        pikachu,
+        level=20,
+    )
+
+    assert spark.hp == spark.max_hp
+
+def test_pokemon_receives_damage_and_looses_hp(pikachu):
+    spark = models.Pokemon(
+        'Spark',
+        pikachu,
+        level=10,
+    )
+
+    spark.receive_damage(10)
+
+    assert spark.max_hp == 27
+    assert spark.hp == 17
+
+def test_pokemon_attacks_other_pokemon_and_makes_damage(pikachu, squirtle):
     thunder_shock = models.Move('Thunder Shock', 40)
-    pikachu = models.Pokemon(
-        'Pikachu',
-        hp=35,
-        attack=55,
-        defense=30,
-        moves=[thunder_shock],
-    )
-    squirtle = models.Pokemon('Squirtle', hp=44, attack=48, defense=65)
 
-    damage = pikachu.perform_move_against(thunder_shock, squirtle)
+    spark = models.Pokemon('Spark', pikachu, level=10, moves=[thunder_shock])
+    bubble = models.Pokemon('Bubble', squirtle, level=10)
 
-    assert damage == 35
-    assert squirtle.hp == 9
+    damage = spark.perform_move_against(thunder_shock, bubble)
+
+    assert damage == 6
+    assert bubble.hp == 22
